0c2050a04349b52f187f9204c19fa53ed966c01b
Kasima Tharnpipitchai <kasima@gmail.com>
2013-04-10 10:02:04 +0200

    Restore alignment functionality

--- hash_rocket.py
+++ hash_rocket.py
@@ -1,6 +1,8 @@
-import sublime, sublime_plugin
+import sublime_plugin
+
 
 class HashRocket(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.edit = edit
-		self.view.insert(edit, self.view.sel()[0].a, " => ")
+
+    def run(self, edit):
+        self.edit = edit
+        self.view.insert(edit, self.view.sel()[0].a, " => ")
